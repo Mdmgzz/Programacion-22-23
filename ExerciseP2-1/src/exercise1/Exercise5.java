2273d2ffb5fb605cfe103ab913e67e1977831f23
mdominguezg <mdominguezg@106-06.info2>
2022-09-26 10:16:14 +0200

    PrimerCommit.

--- ExerciseP2-1/src/exercise1/Exercise5.java
+++ ExerciseP2-1/src/exercise1/Exercise5.java
@@ -15,8 +15,9 @@ public class Exercise5 {
 		largo= sc.nextDouble();
 		area= altura*largo;
 		perimetro= altura*2+largo*2;
-		System.out.print("El área del rectangulo es de "+ area);
-		System.out.println("El perimetro del rectangulo es de "+ perimetro);
+		//mostramos el resultado de las operaciones
+		System.out.println("El área del rectangulo es de "+ area);
+		System.out.print("El perimetro del rectangulo es de "+ perimetro);
 		sc.close();
 
 	}
